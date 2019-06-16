3b58ae48923aaff5a8d33bada01007295fa32e7f
dennis wang <dwang0816@gmail.com>
2019-06-16 03:40:14 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def reformat_languages(languages)
   # your code here
-end
+  hash = {}
+  languages.each do |style,type|
+    type.each do |name, at|
+      at.each do |at, str|
+        if hash[name].nil?
+          hash[name] = {}
+        end
+        if hash[name][:style].nil?
+          hash[name][:style] = []
+        end
+        hash[name][:style] << style
+        hash[name][:type] = str
+      end
+    end
+  end
+  hash
+end 
